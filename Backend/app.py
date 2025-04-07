42a3145f75f2509a9da27fc6a2b0b1c938ab754b
Chloe Najm <chloenajm1@gmail.com>
2025-04-07 02:42:34 -0400

    debugging deployment

--- Backend/app.py
+++ Backend/app.py
@@ -5,14 +5,20 @@ from flask_cors import CORS
 import os
 
 app = Flask(__name__)
-CORS(app)
+
+
+VERCEL_FRONTEND_URL = "https://studio-sync.vercel.app"
+
+
+CORS(app, origins=[VERCEL_FRONTEND_URL], supports_credentials=True)
+
 
 app.register_blueprint(api_bp, url_prefix='/api')
 app.register_blueprint(chat_bp, url_prefix='/chat')
 
 
-
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 4000))
     app.run(debug=False, host='0.0.0.0', port=port)
+
 
